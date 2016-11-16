ecf8bd93e6599cc239f3c6a0d4e43fe7f721af93
jwiesel <Jochen.Wiesel@posteo.de>
2016-11-16 22:00:47 +0100

    Added support for custom UI elements

--- bootstrap4/js/template.js
+++ bootstrap4/js/template.js
@@ -10,8 +10,34 @@
 {
 	$(document).ready(function()
 	{
-            $('.contact-form input').addClass('form-control');
-            $('.contact-form textarea').addClass('form-control');
+			$('.control-group').addClass('form-group');
+            $('.control-group input[type=text]').addClass('form-control');
+            $('.control-group input[type=email]').addClass('form-control');
+            $('.control-group input[type=password]').addClass('form-control');
+            $('.control-group input[type=search]').addClass('form-control');
+            $('.control-group input[type=url]').addClass('form-control');
+            $('.control-group input[type=tel]').addClass('form-control');
+            $('.control-group input[type=number]').addClass('form-control');
+            $('.control-group input[type=date]').addClass('form-control');
+            $('.control-group input[type=datetime-local]').addClass('form-control');
+            $('.control-group input[type=month]').addClass('form-control');
+            $('.control-group input[type=week]').addClass('form-control');
+            $('.control-group input[type=time]').addClass('form-control');
+            $('.control-group input[type=color]').addClass('form-control');
+            $('.control-group select').addClass('form-control');
+            $('.control-group textarea').addClass('form-control');
+            $('.control-group input[type=file]').addClass('form-control-file');
+            //$('.control-group input[type=radio]').addClass('form-check-input'); //Would require reformatting of html-structure
+            $('.control-group input[type=checkbox]').addClass('form-check-input');
+            //Contact form
+            $('.form-group input').addClass('form-control');
+            $('.form-group textarea').addClass('form-control');
+            // Support for bootstrap 4 markup also in custom UI elements, which are designed for Bootstrap 2. 
+            $('table').addClass('table');
+            $('.badge').addClass('tag').addClass('tag-pill');
+            $('.badge-info').addClass('tag-info');
+            $('.label').addClass('tag');
+            $('.label-warning').addClass('tag-warning');
             $('.login input').addClass('form-control');
             //$('*[rel=tooltip]').tooltip()
 
