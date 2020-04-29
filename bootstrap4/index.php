03aa2320be3201cdc781e07165b05f667cd6ee61
Dade <Dade88@users.noreply.github.com>
2020-04-30 00:36:21 +0100

    HTML5 Markup
    Small improvements with HTML5 and SEO.

--- bootstrap4/index.php
+++ bootstrap4/index.php
@@ -21,9 +21,9 @@ include_once (JPATH_ROOT. '/templates/' . $this->template . '/helper.php');
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
 		<link rel="stylesheet" href="templates/bootstrap4/css/template.css">
 		
-		<script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-		<script async src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha256-E/V4cWE4qvAeO5MOhjtGtqDzPndRO1LBk8lJ/PR7CA4=" crossorigin="anonymous"></script>
-		<script async src="templates/bootstrap4/js/template.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+		<script defer src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha256-E/V4cWE4qvAeO5MOhjtGtqDzPndRO1LBk8lJ/PR7CA4=" crossorigin="anonymous"></script>
+		<script defer src="templates/bootstrap4/js/template.js"></script>
 		
 		<?php } ?>
 		<jdoc:include type="head" />
@@ -40,12 +40,12 @@ include_once (JPATH_ROOT. '/templates/' . $this->template . '/helper.php');
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<a class="navbar-brand" href="<?php echo JURI::base(); ?>"><?php echo $app->get('sitename'); ?></a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <nav class="collapse navbar-collapse" id="navbarSupportedContent">
                 <jdoc:include type="modules" name="navbar-1" style="none" />
                 <jdoc:include type="modules" name="navbar-2" style="none" />
-            </div>
+            </nav>
         </header>
-        <div class="body">
+        <main class="body">
             <div class="content">
                 <div class="jumbotron jumbotron-fluid bg-primary text-white">
                     <div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
@@ -72,27 +72,27 @@ include_once (JPATH_ROOT. '/templates/' . $this->template . '/helper.php');
                     <?php endif; ?>
                     <div class="row">
                         <?php if ($this->countModules('sidebar-left')) : ?>
-                            <div id="sidebar" class="col-md-3">
+                            <aside id="sidebar" class="col-md-3">
                                 <div class="sidebar-nav">
                                     <jdoc:include type="modules" name="sidebar-left" style="xhtml" />
                                 </div>
-                            </div>
+                            </aside>
                         <?php endif; ?>
-                        <main id="content" role="main" class="<?php echo $span; ?>">
+                        <article id="content" role="main" class="<?php echo $span; ?>">
                             <jdoc:include type="modules" name="position-3" style="xhtml" />
                             <jdoc:include type="message" />
                             <jdoc:include type="component" />
                             <jdoc:include type="modules" name="position-2" style="none" />
-                        </main>
+                        </article>
                         <?php if ($this->countModules('sidebar-right')) : ?>
-                            <div id="aside" class="col-md-3">
+                            <aside id="aside" class="col-md-3">
                                 <jdoc:include type="modules" name="sidebar-right" style="xhtml" />
-                            </div>
+                            </aside>
                         <?php endif; ?>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
         <footer class="footer bg-faded text-muted" role="contentinfo">
             <hr />
             <div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
